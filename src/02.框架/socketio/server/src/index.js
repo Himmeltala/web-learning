d5a021fb7f162a0297129276feec59e007968606
shiramashiro <zhengrenfu@outlook.com>
2022-08-03 22:19:43 +0800

    笔记本

---
+++ src/02.框架/socketio/server/src/index.js
@@ -0,0 +1,15 @@
+import { Server } from "socket.io";
+
+const server = new Server({
+  cors: {
+    origin: true
+  }
+});
+
+server.on("connection", (socket) => {
+  socket.on("to-server", (e) => {
+    socket.broadcast.emit("transfer", e);
+  });
+});
+
+server.listen(3000);
