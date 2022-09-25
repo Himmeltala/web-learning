64d260cd948a33e3d3f2a31d9b6e39b02ac193ad
Enziandom <zhengrenfu@outlook.com>
2022-09-25 18:24:51 +0800

    :sparkles: rem 和 px 之间的关系

--- src/01.基础/ts/01.type和interface/keyword-type_interface.ts
+++ src/01.基础/ts/01.type和interface/keyword-type_interface.ts
@@ -1,19 +1,23 @@
-// 1. type 可以作为联合类型，而 interface 不可以
+interface Dog {
+  wang(): void;
+}
 
-interface Dog {}
-interface Cat {}
+interface Cat {
+  miao(): void;
+}
 
-// type 可以联合 Dog 以及 Cat 两个接口
 type Animal = Dog | Cat;
 
-// 1. type 不允许重名，interface 重名自动合并
+let animal: Animal = {
+  wang() {},
+  miao() {}
+};
 
-interface Dog1 {
-  name: string;
-}
+let Person = {
+  name: "小明",
+  age: 10
+};
 
-interface Dog1 {
-  age: number;
-}
+type P = typeof Person;
 
-let animal: Dog1 = { name: "dog", age: 2 };
+console.log(typeof Person);
