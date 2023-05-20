75ea91751606248ee6499a0b6e279a46ec3b11c5
Himmelbleu <zhengrenfu@outlook.com>
2023-05-20 23:06:38 +0800

    link source 连接的图形可以实现随便相连

---
+++ 02.framework/vue-learning/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import Test from "./components/Test.vue";
+
+const state = ref(0);
+</script>
+
+<template>
+  <Test />
+</template>
+
+<style scoped></style>
---
+++ 02.framework/vue-learning/src/components/Test.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { reactive, toRef, isRef } from "vue";
+
+const obj = {
+  foo: 1
+};
+
+const foo = toRef(obj, "foo");
+</script>
+
+<template>
+  <div>
+    {{ foo }}
+  </div>
+  <button @click="foo++">Add</button>
+</template>
